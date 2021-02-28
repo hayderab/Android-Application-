23f1d2e46a167b4fc65cdd307631bb6f38211e8a
abbasih3 <abbasih3@coventry.ac.uk>
2021-02-28 22:33:50 +0000

    created class for userdetail wich will be used for login and signup

--- app/src/main/java/com/example/food_app/User.java
+++ app/src/main/java/com/example/food_app/User.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+package com.example.food_app;
 
 public class User {
+
+    String username;
+    String Email;
+    public User(){
+
+
+    }
+
+
+    public User(String UserName, String Email){
+        this.username = UserName;
+        this.Email = Email;
+
+    }
 }
