00aab5edadca379879a055558e7bb5c4e5551bed
abbasih3 <abbasih3@coventry.ac.uk>
2021-03-11 00:46:12 +0000

    rearranged the mainactivity

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.food_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bottom navigation
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+        navView.setOnNavigationItemSelectedListener(navListner);
+
+        // setting a main Fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_layout, new Favourite()).commit();
+
     }
+
+    //Listener  for navigation bar
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner = new
+            BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.homeFragment:
+                            selectedFragment = new Home();
+                            startActivity(new Intent(MainActivity.this, recycleviewList.class));
+                            break;
+                        case R.id.camera_fragment:
+                            //Toast.makeText(MainActivity.this, "Camera ............", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(MainActivity.this, CameraView.class));
+                            selectedFragment = new Camera();
+                            break;
+
+                        case R.id.favouritefragment:
+                            selectedFragment = new Favourite();
+                            break;
+
+                    }
+                    // start of the transaction
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_layout
+                                    ,selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
