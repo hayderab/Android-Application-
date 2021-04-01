aeb008f09cb474112fff3956addb90eb66aa85fe
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-01 23:51:57 +0100

    navigation issues resolved and changed userlocation permission on camera access

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -7,19 +7,33 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.example.food_app.RecycleView.recycleviewList;
+import com.example.food_app.fragments.Camera;
+import com.example.food_app.fragments.Favourite;
+import com.example.food_app.fragments.Home;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private boolean isLocationPremEnabled;
     public static final int PRMISSION_REQUEST_CODE  = 3002;
+    private FusedLocationProviderClient mLocationProviderClient;
+
+    Double lng;
+    Double lat;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-        Fragment fragment = Home.newInstance();
+        Fragment fragment = Camera.newInstance();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_Container, fragment, "Home_fragment");
 
@@ -53,12 +66,17 @@ public class MainActivity extends AppCompatActivity {
                     switch (item.getItemId()){
                         case R.id.homeFragment:
                             selectedFragment = new Home();
-
 //                            startActivity(new Intent(MainActivity.this, recycleviewList.class));
                             break;
                         case R.id.camera_fragment:
                             //Toast.makeText(MainActivity.this, "Camera ............", Toast.LENGTH_SHORT).show();
                             getUserPremission();
+
+//                            if (isLocationPremEnabled){
+//                                getDeviceLocation();
+//                            }
+//                            getUserPremission();
+
 //                            startActivity(new Intent(MainActivity.this, CameraView.class));
                             selectedFragment = new Camera();
                             break;
@@ -78,9 +96,44 @@ public class MainActivity extends AppCompatActivity {
             };
 
 
+
+
+
+    private void getDeviceLocation(){
+        Log.d("TAG", "getDeviceLocation: Getting device location");
+        mLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+                if(isLocationPremEnabled){
+                    Task location = mLocationProviderClient.getLastLocation();
+                    location.addOnCompleteListener(new OnCompleteListener(){
+                        @Override
+                        public void onComplete(@NonNull Task task){
+                            if(task.isSuccessful()){
+                                Log.d("TAG", "onComplete: found location");
+                                Location currentLocation = (Location) task.getResult();
+                                Log.d("TAG", "current user location: " + currentLocation );
+                                 lng = currentLocation.getLatitude();
+                                 lat = currentLocation.getLongitude();
+                                saveData();
+                                Log.d("TAG", "current user location: " + lat +  lng);
+
+
+                            }else{
+                                Log.d("TAG", "onComplete: Current Location is null");
+                            }
+                        }
+                    });
+                }
+            }
+        catch(SecurityException e){
+                Log.e("TAG","getDeviceLocation: SecurityExcetion" + e.getMessage() );
+            }
+    }
+
     private void getUserPremission() {
         if (isLocationPremEnabled){
             Toast.makeText(this, "readyMap", Toast.LENGTH_SHORT).show();
+
         } else {
             // Checking if we got GPS permission
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -98,12 +151,24 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PRMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED){
             isLocationPremEnabled  = true;
-            startActivity(new Intent(MainActivity.this, CameraView.class));
-
+            getDeviceLocation();
+//            startActivity(new Intent(MainActivity.this, CameraView.class));
             Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
         }else {
             Toast.makeText(this, "Permission not Granted", Toast.LENGTH_SHORT).show();
 
         }
     }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LocationSharedPrefers", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("lat", Double.toString(lat));
+        editor.putString("lng", Double.toString(lng));
+        Log.d("TAG", "saveData: " +  lat);
+        editor.commit();
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
