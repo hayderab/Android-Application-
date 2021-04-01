23629a1de002b204bdec9518084ac52cf96ff1d4
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-02 00:17:16 +0100

    implemented functionalities for favourite button

--- app/src/main/java/com/example/food_app/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/food_app/RecyclerViewAdapter.java
@@ -1,28 +1,26 @@
 package com.example.food_app;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.food_app.RecycleView.Fooditems;
+import com.example.food_app.fragments.Favourite;
 import com.squareup.picasso.Picasso;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,10 +60,10 @@ public class RecyclerViewAdapter  extends RecyclerView.Adapter<RecyclerViewAdapt
         final String imageUrl = currentItem.getPhotoReference();
         final ArrayList<String> geomatry = currentItem.getGeomatry();
 
-        Log.d("TAG", "onBindViewHolder: data test ..." + geomatry);
+        Log.d("TAG", "onBindViewHolder: data favourite_View ..." + geomatry);
 //        final ArrayList<Array> new  = currentItem.getGeomatry();
 
-        String placeName = currentItem.getName();
+        final String placeName = currentItem.getName();
         final String Address = currentItem.getAddress();
 
         holder.titleTextView.setText(placeName);
@@ -93,7 +91,7 @@ public class RecyclerViewAdapter  extends RecyclerView.Adapter<RecyclerViewAdapt
             @Override
             public void onClick(View view) {
                 // data to be used in other activity
-                Log.d("TAG", "onClick: test " + Address );
+                Log.d("TAG", "onClick: favourite_View " + Address );
                 Intent intent  = new Intent(context, Map.class);
                 intent.putExtra("places_name",Address);
                 intent.putExtra("lat",geomatry.get(0));
@@ -105,18 +103,36 @@ public class RecyclerViewAdapter  extends RecyclerView.Adapter<RecyclerViewAdapt
         });
 
         holder.favouritebtn.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 // data to be used in other activity
-                Log.d("TAG", "onClick: favourite button clicked"  );
-//                Intent intent  = new Intent(context, Map.class);
-////                intent.putExtra("places_name",Address);
-////                intent.putExtra("lat",geomatry.get(0));
-////                intent.putExtra("lng",geomatry.get(1));
+//                Log.d("TAG", "onClick: favourite button clicked"  );
+//                Intent intent  = new Intent(context, favourite_View.class);
+//                intent.putExtra("title", placeName);
+//                intent.putExtra("url", imageUrl);
+//                intent.putExtra("places_name",Address);
+//                intent.putExtra("lat",geomatry.get(0));
+//                intent.putExtra("lng",geomatry.get(1));
 //
 //                context.startActivity(intent);
+                AppCompatActivity activity = (AppCompatActivity)view.getContext();
+                Fragment fragment = new Favourite();
+                Bundle args = new Bundle();
+                args.putString("title", placeName);
+                args.putString("url", imageUrl);
+                args.putString("places_name",Address);
+                args.putString("lat",geomatry.get(0));
+                args.putString("lng",geomatry.get(1));
+                fragment.setArguments(args);
 
+//                FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+//                transaction.replace(R.id.fav_frame_Container, fragment, "favourite_fragment");
+
+                activity.getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_layout, fragment, "favourite_fragment")
+                        .commit();
+
+                Log.d("TAG", "onClick: " + "Favourite fragment");
             }
         });
 
@@ -146,13 +162,12 @@ public class RecyclerViewAdapter  extends RecyclerView.Adapter<RecyclerViewAdapt
 
         public MyViewHolder(View view, OnCardListener onCardListener) {
             super(view);
-            titleTextView = view.findViewById(R.id.textTitle);
-            textDisciption = view.findViewById(R.id.textDesc);
-            ImageUrl = view.findViewById(R.id.imageView);
+            titleTextView = view.findViewById(R.id.favTitle);
+            textDisciption = view.findViewById(R.id.favDesc);
+            ImageUrl = view.findViewById(R.id.favImg);
 
             parent_layout = itemView.findViewById(R.id.parent_layout);
             favouritebtn = itemView.findViewById(R.id.favouritebtn);
-
             this.onCardListener = onCardListener;
 
             itemView.setOnClickListener(this);
