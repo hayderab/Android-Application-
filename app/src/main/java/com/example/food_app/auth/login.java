879955e8e1ae7665b04d3b290bcacf33b62ff694
abbasih3 <abbasih3@coventry.ac.uk>
2021-04-08 00:16:29 +0100

    added onClick listner for buttons, fixed the problem with switch statement.

--- app/src/main/java/com/example/food_app/auth/login.java
+++ app/src/main/java/com/example/food_app/auth/login.java
@@ -20,13 +20,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class login extends AppCompatActivity implements View.OnClickListener  {
+public class login extends AppCompatActivity {
 
-     TextView login_button;
+     TextView login_button,forgottePw , createAccount;
       EditText email, password;
      String Email, Password;
 //    private Button login_button;
-    private FirebaseAuth mAuth;
+     FirebaseAuth mAuth;
     ProgressBar progressBar;
 
     @Override
@@ -40,15 +40,20 @@ public class login extends AppCompatActivity implements View.OnClickListener  {
         //create_users  = (TextView)findViewById(R.id.create_account);
 
         login_button = (Button)findViewById(R.id.login_btn);
-        login_button.setOnClickListener(this);
-//        login_button.setOnClickListener(new View.OnClickListener(){
-//
-//
-//            @Override
-//            public void onClick(View v) {
-//                btn_longIn();
-//            }
-//        });
+        login_button = (Button)findViewById(R.id.login_btn);
+
+        forgottePw = findViewById(R.id.forget_password);
+        createAccount = findViewById(R.id.create_account);
+
+
+
+//        login_button.setOnClickListener(this);
+        login_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                btn_longIn();
+            }
+        });
 
         email = (EditText) findViewById(R.id.inputEmail);
         password = (EditText) findViewById(R.id.input_password);
@@ -60,33 +65,36 @@ public class login extends AppCompatActivity implements View.OnClickListener  {
             finish();
         }
 
+        // Set event trigger for forgotten password button
+        forgottePw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Run function after event trigger
+                Intent intent2 = new Intent(login.this, forgotPassword.class);
+                startActivity(intent2);
+            }
+        });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Run function after event trigger
+                Intent intent = new Intent(login.this, signup.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
-//
+
+
     public void btn_register(View view) {
         Intent intent = new Intent(login.this, signup.class);
         startActivity(intent);
     }
 
-//    public void btn_longIn(View, view) {
-//
-//    }
-//
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.create_account:
-                Intent intent = new Intent(login.this, signup.class);
-                startActivity(intent);
-                break;
-            case R.id.login_btn:
-                btn_longIn();
-                break;
 
-        }
-
-    }
 
     public void btn_longIn() {
         String Email = email.getText().toString();
@@ -115,9 +123,7 @@ public class login extends AppCompatActivity implements View.OnClickListener  {
         }
 
         //progressBar.setVisibility(View.VISIBLE);
-
         mAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
-
             @Override
             public void onComplete(@NonNull Task<AuthResult> task)
 
@@ -138,55 +144,6 @@ public class login extends AppCompatActivity implements View.OnClickListener  {
 
     }
 
-//    public void login_btn(View view) {
-//        String Email = email.getText().toString().trim();
-//        String Password = password.getText().toString().trim();
-//
-//        if (Email.isEmpty()) {
-//            email.setError("Email is required!");
-//            email.requestFocus();
-//            return;
-//        }
-//
-//        if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
-//            email.setError("Please provide valid email!");
-//            email.requestFocus();
-//            return;
-//        }
-//        if (Password.isEmpty()) {
-//            password.setError("Password is required!");
-//            password.requestFocus();
-//
-//        }
-//        if (Password.length() < 8) {
-//            password.setError("Minimum password length should be 8");
-//            password.requestFocus();
-//            return;
-//        }
-//
-//        progressBar.setVisibility(View.VISIBLE);
-//
-//        mauth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
-//
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task)
-//
-//            {
-//
-//                if (task.isSuccessful()){
-//                    //register user
-//                    startActivity (new Intent (login.this, MainActivity.class));
-//                }else{
-//
-//                    Toast.makeText(login.this, "Failed to login! Please Check your credentials", Toast.LENGTH_LONG).show();
-//                }
-//
-//            }
-//
-//        });
-//
-//
-//    }
 }
 
 
